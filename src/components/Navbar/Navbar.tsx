0353115c6bf74cef1988100b5018d63ca84b6b9a
abbasnosrati <abbas.nosrati68@gmail.com>
2024-12-12 11:39:32 +0330

    set menuItems in constants

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,7 @@
 import { useEffect, useRef, useState } from "react";
+import { menuItems } from "../../constants/constants";
 
 const Navbar = () => {
-  const menuItems = [
-    { id: "0", title: "What is Muon", subItems: [] },
-    {
-      id: "1",
-      title: "Resources",
-      subItems: ["Docs", "Articles", "Lightpaper"],
-    },
-    { id: "2", title: "Products", subItems: [] },
-    { id: "3", title: "About us", subItems: [] },
-  ];
-
   const [hoveredMenu, setHoveredMenu] = useState(null);
 
   const handleMouseEnter = (index: any) => {
