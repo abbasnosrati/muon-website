679b29dd0c5a3eca63a9a13ec59758e82637df59
abbasnosrati <abbas.nosrati68@gmail.com>
2024-12-12 11:38:51 +0330

    create constant file

---
+++ src/constants/constants.ts
@@ -0,0 +1,10 @@
+export const menuItems = [
+  { id: "0", title: "What is Muon", subItems: [] },
+  {
+    id: "1",
+    title: "Resources",
+    subItems: ["Docs", "Articles", "Lightpaper"],
+  },
+  { id: "2", title: "Products", subItems: [] },
+  { id: "3", title: "About us", subItems: [] },
+];
