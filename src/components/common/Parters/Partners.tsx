da740e1ce4f2e541dfa61d8b21808b1bac9e308b
abbasnosrati <abbas.nosrati68@gmail.com>
2024-12-14 20:11:21 +0330

    create marquee

--- src/components/common/Parters/Partners.tsx
+++ src/components/common/Parters/Partners.tsx
@@ -1,32 +1,123 @@
+import { useEffect } from "react";
+
 const Partners = () => {
+  useEffect(() => {
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      const scrolls = document.querySelectorAll(".scroller");
+      addAnimation(scrolls);
+    }
+  }, []);
+
+  const addAnimation = (scrolls: any) => {
+    scrolls.forEach((scroller: any) => {
+      scroller.setAttribute("data-animated", "true");
+    });
+  };
+
   return (
-    <div className="logos absolute left-0 right-0">
-      <div className="logo_items">
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
-        <img src="https://navneetdwivedi.github.io/Logo_Slider/logo.png" />
+    <div className="scroller absolute left-0">
+      <div className="tag_list scroller__inner">
+        <div className="img-container zelullar_img  ">
+          <div className="img-box min-w-[130px] h-[20px] ">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/thena.svg" alt="" />
+          </div>
+        </div>
+        <div className="img-container other-images">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/symmio.svg" alt="" />
+          </div>
+        </div>
+
+        <div className="img-container zelullar_img ">
+          <div className="img-box min-w-[130px] min-h-[20px]">
+            <img src="./assets/images/partners/zellular logo.svg" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
