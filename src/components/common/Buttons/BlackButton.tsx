4d25038070d0b7f7b75b1168b1c63c017673bff1
abbasnosrati <abbas.nosrati68@gmail.com>
2025-04-07 16:58:08 +0330

    fixed title style

--- src/components/common/Buttons/BlackButton.tsx
+++ src/components/common/Buttons/BlackButton.tsx
@@ -4,7 +4,7 @@ interface BtnProps {
 }
 const BlackButton = ({ btnText, btnNum }: BtnProps) => {
   return (
-    <div className="text-whiteTextSecond px-[30px] flex justify-between above-1440:opacity-100 opacity-90 tracking-[2px]  select-none !leading-[18px] text-sm lg:text-base above-1440:text-[20px] font-semibold font-azeretMono border border-darkText mt-4 h-[47px] above-1440:h-[60px] w-full max-w-[364px] above-1440:max-w-[484px] items-center relative">
+    <div className="text-whiteTextSecond px-[30px] flex justify-between above-1440:opacity-100 opacity-90 tracking-[2px]  select-none !leading-[18px] text-sm lg:text-base above-1440:text-[20px] font-medium font-azeretMono border border-darkText mt-4 h-[47px] above-1440:h-[60px] w-full max-w-[364px] above-1440:max-w-[484px] items-center relative">
       <div>{btnNum}//</div>
       <div>{btnText}</div>
       {/* <img
