54db01476395d18dca9d5e9cdadb204a68e531f4
abbasnosrati <abbas.nosrati68@gmail.com>
2025-04-13 10:21:48 +0330

    fixed footer items

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,46 @@
 import { menuItems, scrollToSection } from "../../constants/constants";
 
+const footerItems = [
+  {
+    id: "0",
+    title: "What is Muon",
+    subItems: [],
+    src: "https://docs.muon.net/muon-protocol/what-is-muon/elevator-pitch",
+  },
+  {
+    id: "1",
+    title: "Docs",
+    src: "https://docs.muon.net/muon-protocol",
+    subItems: [],
+  },
+  {
+    id: "2",
+    title: "Litepaper",
+    src: "https://files.muon.net/Muon-Litepaper-v01.pdf",
+    subItems: [],
+  },
+  { id: "3", title: "Ecosystem", subItems: [], src: "4" },
+  // { id: "3", title: "About us", subItems: [], src: "" },
+  {
+    id: "4",
+    title: "Dashboard",
+    subItems: [],
+    src: "https://app.muon.net/",
+  },
+  {
+    id: "5",
+    title: "Socials",
+    subItems: [
+      { title: "X", src: "https://x.com/muon_net" },
+      { title: "Discord", src: "https://discord.gg/muonnetwork" },
+      { title: "Medium", src: "https://medium.com/muon" },
+      { title: "Github", src: "https://Github.com/muon-protocol" },
+      { title: "Telegram", src: "https://t.me/muon_net" },
+    ],
+    src: "",
+  },
+];
+
 const Footer = () => {
   return (
     <div className="font-azeretMono pb-[30px]">
@@ -13,7 +54,7 @@ const Footer = () => {
         </div>
         <div className="left-side text-[12px] above-1024:text-base above-1440:text-[20px] font-light !leading-[18px] above-1024:pl-[140px] pt-[20px] text-darkText">
           <div className="flex flex-col gap-[18px] above-1440:gap-[24px] below-1024:gap-[10px] above-1440:mt-14">
-            {menuItems.map((item, index) => (
+            {footerItems.map((item, index) => (
               <div className="flex" key={index}>
                 {item.title !== "Socials" && (
                   <div className="flex items-center">
